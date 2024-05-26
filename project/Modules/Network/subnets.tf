6f3b5c8ffaaa14ae262ee5168eae9cc1a90fc502
RowanHomam <161208993+RowanHomam@users.noreply.github.com>
2024-05-26 20:11:31 +0300

    final edit

--- project/Modules/Network/subnets.tf
+++ project/Modules/Network/subnets.tf
@@ -1,7 +1,6 @@
 resource "aws_subnet" "Publicsubnet" {
   vpc_id     = aws_vpc.network.id
   cidr_block = "10.0.1.0/24"
-#   availability_zone = "us-east-1a"
   tags = {
     Name = "Pub_subnet"
   }
